2db4128f0bfcf8fe15dd302f0ac2289a42db0e95
Mac <mac@Edit1East.local>
2019-01-15 17:06:35 -0500

    first commit

--- src/web.entry.js
+++ src/web.entry.js
@@ -1,8 +1,17 @@
-import Vue from 'vue'
+import Vue from 'vue';
+import './style.scss';
+import moment from 'moment-timezone';
+import App from './components/App.vue';
+
+moment.tz.setDefault('UTC');// Sets TimeZone
+Object.defineProperty(Vue.prototype, '$moment', { get() { return this.$root.moment } });
 
 new Vue({
   el: '#app',
-  data: {
-    msg: 'Hello World'
+    data: {
+      moment
+    },
+  components: {
+    App
   }
 });
