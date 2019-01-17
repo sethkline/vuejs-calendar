226e4a7dede139f297abecd09d378c2bb05fbc08
Mac <mac@Edit5-2.local>
2019-01-17 11:13:47 -0500

    event form

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import moment from 'moment-timezone';
+
 
 Vue.use(Vuex);
+moment.tz.setDefault('UTC');
 
 export default new Vuex.Store({
         state: {
@@ -9,7 +12,15 @@ export default new Vuex.Store({
           currentMonth: 1,
           eventFormPositionX: 0,
           eventFormPositionY: 0,
+          eventFormActive: false,
+          events: [
+                {description: 'Random event 1', date: moment('2019-01-18', 'YYYY-MM-DD') },
+                {description: 'Random event 2', date: moment('2019-02-18', 'YYYY-MM-DD') },
+                {description: 'Random event 3', date: moment('2019-01-15', 'YYYY-MM-DD') },
+          ],
+          eventFormDate: moment(),
         },
+
         mutations: {
             setCurrentMonth(state, payload) {
                 state.currentMonth = payload;
@@ -20,6 +31,18 @@ export default new Vuex.Store({
             eventFormPosition(state, payload) {
                 state.eventFormPositionX = payload.x
                 state.eventFormPositionY = payload.y
+            },
+            eventFormActive(state, payload) {
+                state.eventFormActive = payload;
+            },
+            addEvent(state, payload) {
+                state.events.push({
+                    description: payload,
+                    date: state.eventFormDate
+                })
+            },
+            eventFormDate(state, payload) {
+                state.eventFormDate = payload;
             }
         }
       });
